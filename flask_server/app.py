39ca06d15f92d2d7073a9c2fd0fa4017e4259b13
c-cys <choiys071222@gmail.com>
2025-08-05 23:43:23 +0900

    ADD Detection

---
+++ flask_server/app.py
@@ -0,0 +1,20 @@
+from flask import Flask, send_file, jsonify
+import predict
+
+app = Flask(__name__)
+
+@app.route('/run', methods=['GET'])
+def run_code():
+    detection, _ = predict.run()
+    return jsonify({
+        'detection': detection,
+        'image_url': 'http://localhost:5000/image'
+    })
+
+@app.route('/image')
+def get_image():
+    _, result_path = predict.run()
+    return send_file(result_path)
+
+if __name__ == '__main__':
+    app.run(host='localhost', port=5000, debug=True)
---
+++ flask_server/predict.py
@@ -0,0 +1,20 @@
+from ultralytics import YOLO
+
+def run():
+    model = YOLO('best.pt')
+
+    file_name = 'test250805.jpg'
+    result = model(file_name)
+
+    result_name = f'detection/result_{file_name}'
+    result[0].save(filename=result_name)
+
+    detections = []
+    for box in result[0].boxes:
+        detections.append({
+            'class_id': int(box.cls[0]),
+            'confidence': float(box.conf[0]),
+            'bbox': [round(x, 1) for x in box.xyxy[0].tolist()]
+        })
+
+    return detections, result_name
