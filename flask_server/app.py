15287ddb60a22f2f898e321e1840f47b6e539252
c-cys <choiys071222@gmail.com>
2025-08-07 00:26:25 +0900

    NOTHING

--- flask_server/app.py
+++ flask_server/app.py
@@ -1,16 +1,11 @@
-<<<<<<< HEAD
+
 from flask import Flask, send_file, jsonify
 import predict
-=======
-from flask import Flask, jsonify
-import hello
->>>>>>> 997aa603fb96247d7c7c4984c294c885563ef2f5
 
 app = Flask(__name__)
 
 @app.route('/run', methods=['GET'])
 def run_code():
-<<<<<<< HEAD
     detection, _ = predict.run()
     return jsonify({
         'detection': detection,
@@ -24,10 +19,3 @@ def get_image():
 
 if __name__ == '__main__':
     app.run(host='localhost', port=5000, debug=True)
-=======
-    result = hello.run()
-    return jsonify({'result': result})
-
-if __name__ == '__main__':
-    app.run(host='localhost', port=5000, debug=True)
->>>>>>> 997aa603fb96247d7c7c4984c294c885563ef2f5
